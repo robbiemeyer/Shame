32b482e1cee9c543525f6d2a7974fe62c39e4887
ArthurRab <ArthurRabinovich1@gmail.com>
2017-09-16 13:35:10 -0400

    Testing

--- app/src/main/java/io/arro/shame/HomeActivity.java
+++ app/src/main/java/io/arro/shame/HomeActivity.java
@@ -45,6 +45,7 @@ public class HomeActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+            //qq
         }
 
         return super.onOptionsItemSelected(item);
